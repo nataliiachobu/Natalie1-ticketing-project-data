19c81ba421112333723dd7f10eace8b048640843
nataliiachobu <mebel2008@yahoo.com>
2022-10-31 19:12:42 -0700

    Initial commit

---
+++ src/main/java/com/cydeo/Natalie1TicketingProjectDataApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Natalie1TicketingProjectDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Natalie1TicketingProjectDataApplication.class, args);
+    }
+
+}
